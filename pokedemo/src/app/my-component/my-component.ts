8b050aa203a3a4adff2f09e5518fe647aebc3c23
Orulirn <ewencarre@free.fr>
2025-10-08 11:08:10 +0200

    Q8-Q9

--- pokedemo/src/app/my-component/my-component.ts
+++ pokedemo/src/app/my-component/my-component.ts
@@ -9,7 +9,8 @@ import { Pokemon } from '../pokemon';
 })
 export class MyComponent {
   pokemon_id: string = '';
-  selected_pokemon: number = 0;
+  selected_pokemon: number= 0;
+  pipe_text: string = '';
 
   pokemonList: Pokemon[] = [
     { id: 1, nom: 'Pikachu' },
@@ -18,4 +19,16 @@ export class MyComponent {
     { id: 4, nom: 'Carapuce' },
     { id: 5, nom: 'Roucool' }
   ];
+
+  onGo() {
+    const pokemonId = Number(this.selected_pokemon);
+    const selectedPokemon = this.pokemonList.find(p => p.id === pokemonId);
+  
+    if (selectedPokemon) {
+      console.log('Pokémon sélectionné - ID:', selectedPokemon.id);
+      console.log('Pokémon sélectionné - Nom:', selectedPokemon.nom);
+    } else {
+      console.log('Aucun pokémon sélectionné');
+    }
+  }
 }
